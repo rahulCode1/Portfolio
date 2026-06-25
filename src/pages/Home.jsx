6954921800aa9e177ae6fb757ff81d7fc2b0c589
Rahul <pbrkumawat@gmail.com>
2026-06-25 12:24:34 +0530

    Update resume link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ const Home = () => {
             </button>
 
             <a
-              href="https://drive.google.com/file/d/11S63eb0K_VCzXXKjp70KhMGYFWKFTM-V/view?usp=sharing"
+              href="https://drive.google.com/file/d/1mtf2Gztoznzsnh039aVCA1PKb-9uSeXl/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="btn btn-outline-secondary"
